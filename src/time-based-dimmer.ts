b554e61ea115ad9c09a502354228546b7d72b0db
Johannes Herrmann <j.herrmann@cis-solutions.eu>
2019-12-29 10:25:14 +0100

    use typescript class

--- src/time-based-dimmer.ts
+++ src/time-based-dimmer.ts
@@ -12,23 +12,23 @@ interface TimeBasedDimmerConfig extends NodeProperties {
 }
 
 module.exports = function(red: Red): void {
-  function TimeBasedDimmerNode(config: TimeBasedDimmerConfig) {
-    function tick(send: Function, node: Node) {
+  class TimeBasedDimmerNode {
+    tick(send: Function, node: Node) {
       let newValue: number
       const oldValue = node.context().get('value') || 0
       if (node.context().get('mode') === 'inc') {
-        newValue = oldValue + config.step
-        if (newValue > config.maxValue) {
+        newValue = oldValue + this.config.step
+        if (newValue > this.config.maxValue) {
           clearInterval(node.context().get('timer'));
           node.context().set('timer', null)
-          newValue = config.maxValue;
+          newValue = this.config.maxValue;
         }
       } else {
-        newValue = oldValue - config.step
-        if (newValue < config.minValue) {
+        newValue = oldValue - this.config.step
+        if (newValue < this.config.minValue) {
           clearInterval(node.context().get('timer'));
           node.context().set('timer', null)
-          newValue = config.minValue;
+          newValue = this.config.minValue;
         }
       }
       if (node.context().get('value') === newValue) return
@@ -37,45 +37,48 @@ module.exports = function(red: Red): void {
       send({ payload: newValue })
     }
     
-    red.nodes.createNode(this, config);
-    var node = this;
-    node.on('input', (msg: any, send: Function, done: Function) => {
-      switch(typeof msg.payload) {
-        case 'number':
-          node.status({ fill:"grey", shape:"dot", text: msg.payload.toString() });
-          node.context().set('value', msg.payload)
-          send(msg)
-        break
-        case 'string':
-          const timer = node.context().get('timer');
-          switch(msg.payload) {
-            case config.startIncCommand:
-              if (timer) break
-              node.context().set('mode', 'inc')
-              node.context().set('timer', setInterval(() => tick(send, node), config.interval));
-            break
-            case config.startDecCommand:
-              if (timer) break
-              node.context().set('mode', 'dec')
-              node.context().set('timer', setInterval(() => tick(send, node), config.interval));
-            break
-            case config.stopIncCommand:
-            case config.stopDecCommand:
-              if (!timer) break
-              clearInterval(timer);
-              node.context().set('timer', null)
-            break
-            default:
-              node.warn(`missing command "${msg.payload}"`)
-          }
-        break
-        default:
-          // do nothing
-      }
-      if (done) {
-        done()
-      }
-    });
+    constructor(private config: TimeBasedDimmerConfig) {
+      red.nodes.createNode(this as any, config);
+      var node: Node = this as any;
+      node.on('input', (msg: any, send: Function, done: Function) => {
+        switch(typeof msg.payload) {
+          case 'number':
+            node.status({ fill:"grey", shape:"dot", text: msg.payload.toString() });
+            node.context().set('value', msg.payload)
+            send(msg)
+          break
+          case 'string':
+            const timer = node.context().get('timer');
+            switch(msg.payload) {
+              case config.startIncCommand:
+                if (timer) break
+                node.context().set('mode', 'inc')
+                node.context().set('timer', setInterval(() => this.tick(send, this as any), config.interval));
+              break
+              case config.startDecCommand:
+                if (timer) break
+                node.context().set('mode', 'dec')
+                node.context().set('timer', setInterval(() => this.tick(send, this as any), config.interval));
+              break
+              case config.stopIncCommand:
+              case config.stopDecCommand:
+                if (!timer) break
+                clearInterval(timer);
+                node.context().set('timer', null)
+              break
+              default:
+                node.log(`missing command "${msg.payload}"`)
+            }
+          break
+          default:
+            // do nothing
+        }
+        if (done) {
+          done()
+        }
+      });
+    }
   }
-  red.nodes.registerType("time-based-dimmer", TimeBasedDimmerNode);
+
+red.nodes.registerType("time-based-dimmer", TimeBasedDimmerNode as any);
 }
